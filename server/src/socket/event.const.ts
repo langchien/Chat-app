45fc79841876808743f48e7d443e234bc81772a4
langchien <langtienk4@gmail.com>
2025-12-22 00:12:36 +0700

    Update(UI): Check isAuthentication trong clientLoader để tránh hiện tượng nhấp nháy ui

--- server/src/socket/event.const.ts
+++ server/src/socket/event.const.ts
@@ -1,6 +1,5 @@
 export const SOCKET_EVENTS = {
   ONLINE_USERS: 'online-users',
-  SEND_MESSAGE: 'send-message',
   RECEIVE_MESSAGE: 'receive-message',
   MEDIA_PROCESSING_UPDATE: 'media-processing-update',
   UPDATE_CHAT: 'update-chat',

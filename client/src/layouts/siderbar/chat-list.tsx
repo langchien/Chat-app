9fa59559ab1028a76917377bf764eedad7f08a3b
nguyendinhluong20225878 <luong09876789@gmail.com>
2025-12-24 11:14:54 +0700

    Feat(AuthenticatedLayout): Thêm phần xử lý fetch chat và user profile luôn là mới nhất khi tải lại trang

--- client/src/layouts/siderbar/chat-list.tsx
+++ client/src/layouts/siderbar/chat-list.tsx
@@ -2,6 +2,7 @@ import { Item, ItemContent, ItemMedia, ItemTitle } from '@/components/ui/item'
 import { SidebarContent } from '@/components/ui/sidebar'
 import { Spinner } from '@/components/ui/spinner'
 import { useChatList } from '@/features/chat/hooks/use-chat-list'
+import type { IChatPaginateCursorResDto } from '@/features/chat/services'
 import { EmptyChat } from '@/layouts/siderbar/empty-chat'
 import { SidebarDirectChat } from '@/layouts/siderbar/sidebar-direct-chat'
 import { SidebarGroupChat } from '@/layouts/siderbar/sidebar-group-chat'
@@ -28,24 +29,24 @@ function EndChat({ hidden }: { hidden?: boolean }) {
   )
 }
 
-export function ChatList() {
-  const { data, chatGroups, chatDirects, chatId, getMore, hasMore } = useChatList()
+export function ChatList({ paginateData }: { paginateData: IChatPaginateCursorResDto }) {
+  const { chatList, chatGroups, chatDirects, chatId, getMore, hasMore } = useChatList(paginateData)
   return (
     <div
       id='scrollableConversationList'
       className='app-scroll h-full flex-1 overflow-y-auto min-h-0'
     >
       <InfiniteScroll
-        dataLength={data.length}
+        dataLength={chatList.length}
         next={getMore}
         hasMore={hasMore}
         scrollThreshold={0.8}
         loader={<Loading />}
-        endMessage={<EndChat hidden={data.length === 0} />}
+        endMessage={<EndChat hidden={chatList.length === 0} />}
         scrollableTarget='scrollableConversationList'
       >
         <SidebarContent>
-          {data.length === 0 ? (
+          {chatList.length === 0 ? (
             <EmptyChat />
           ) : (
             <>
